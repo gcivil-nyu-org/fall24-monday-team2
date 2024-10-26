b5952ba8c5106e56dac37a8f98efc093acd655fe
Rohan Chopra <rohanchopra96@yahoo.com>
2024-10-26 09:59:09 -0400

    DB removed

--- FitOn/settings.py
+++ FitOn/settings.py
@@ -116,12 +116,12 @@ WSGI_APPLICATION = 'FitOn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
