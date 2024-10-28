15376a439164b51075c592799b2465f5e15d9f3c
Rohan Chopra <rohanchopra96@yahoo.com>
2024-10-28 16:02:14 -0400

    Fixed Travis Errors

--- FitOn/test.py
+++ FitOn/test.py
@@ -95,7 +95,7 @@ class ForumTests(TestCase):
         user_ids = [user["username"] for user in users]
         self.assertIn("test_user", user_ids)
         self.assertIn("another_user", user_ids)
-        self.assertEqual(len(users), 4)  # Five unique users
+        self.assertEqual(len(users), 5)  # Five unique users
 
     def test_forum_view(self):
         response = self.client.get(reverse("forum"))
